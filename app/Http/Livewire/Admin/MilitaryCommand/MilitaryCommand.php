039ea64e22991f1d3a8f336756b464af6d47dcac
gabriellmeireles <gabriellmeireles@hotmail.com>
2022-11-28 19:23:40 -0300

    finalizando o CRUD do Comando Militar

--- app/Http/Livewire/Admin/MilitaryCommand/MilitaryCommand.php
+++ app/Http/Livewire/Admin/MilitaryCommand/MilitaryCommand.php
@@ -13,6 +13,8 @@ class MilitaryCommand extends Component
      * DataTable config
      */
 
+    public $full_name, $short_name, $status;
+
     use WithPagination;
 
     protected $paginationTheme = 'bootstrap';
@@ -30,6 +32,96 @@ class MilitaryCommand extends Component
         $this->resetPage();
     }
 
+    protected $rules = [
+        'full_name' => 'required|string',
+        'short_name' => 'required|string',
+    ];
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    protected $listeners = [
+        'resetForm'
+    ];
+
+    public function resetForm()
+    {
+        $this->full_name = $this->short_name = $this->status = null;
+        $this->resetErrorBag();
+    }
+
+    public function create()
+    {
+        $this->validate();
+        try {
+            $cma = new ModelsMilitaryCommand();
+            $cma->full_name = $this->full_name;
+            $cma->short_name = $this->short_name;
+            $cma->status = 1;
+            $cma->save();
+
+            $this->showEventMessage('Cadastrado realizado com sucesso.', 'success');
+            $this->dispatchBrowserEvent('hideAddCmaModal');
+        } catch (\Illuminate\Database\QueryException $eQuery) {
+            $this->showEventMessage('Não foi possível realizar o cadastrar. <br><strong>Exceção: Banco de Dados</strong>!', 'error');
+        }
+    }
+
+    public function edit($cma)
+    {
+        $this->cma_id = $cma['id'];
+        $this->full_name = $cma['full_name'];
+        $this->short_name = $cma['short_name'];
+        $this->status = $cma['status'];
+
+        $this->dispatchBrowserEvent('showEditCmaModal');
+    }
+
+    public function update()
+    {
+        $this->validate();
+        try {
+            $cma = ModelsMilitaryCommand::find($this->cma_id);
+            $cma->full_name = $this->full_name;
+            $cma->short_name = $this->short_name;
+            $cma->status = $this->status;
+            $cma->save();
+
+            $this->showEventMessage('Alteração realizada com sucesso.','success');
+            $this->dispatchBrowserEvent('hideEditCmaModal');
+        } catch (\Illuminate\Database\QueryException $eQuery) {
+            $this->showEventMessage('Não foi possível realizar a alteração. <br><strong>Exceção: Banco de Dados</strong>!', 'error');
+            $this->dispatchBrowserEvent('hideEditCmaModal');
+       }
+    }
+
+    public function delete($cma)
+    {
+        $this->cma_id = $cma['id'];
+        $this->full_name = $cma['full_name'];
+
+        $this->dispatchBrowserEvent('showDeleteCmaModal');
+    }
+
+    public function destroy()
+    {
+        try {
+            $cma = ModelsMilitaryCommand::find($this->cma_id);
+            $cma->update([
+                'status' => 0,
+            ]);
+
+            $this->showEventMessage('Desativação realizada com sucesso', 'success');
+            $this->dispatchBrowserEvent('hideDeleteCmaModal');
+        } catch (\Illuminate\Database\QueryException $eQuery) {
+            $this->showEventMessage('Problema ao desativar o usuário <br><strong>Exceção Bando de Dados!<strong>' ,'error');
+            $this->dispatchBrowserEvent('hideDeleteCmaModal');
+        }
+    }
+
+
     public function render()
     {
         return view('livewire.admin.military-command.military-command', [
@@ -38,4 +130,13 @@ class MilitaryCommand extends Component
                                         ->paginate($this->per_page),
         ]);
     }
+
+    public function showEventMessage($message, $type)
+    {
+        return $this->dispatchBrowserEvent('showEventMessage',[
+            'message' => $message,
+            'type' => $type,
+        ]);
+    }
+    
 }
--- resources/views/admin/military-command/index.blade.php
+++ resources/views/admin/military-command/index.blade.php
@@ -3,3 +3,33 @@
 @section('content')
     @livewire('admin.military-command.military-command')
 @endsection
+
+@push('scripts')
+  <script>
+    $(window).on('hidden.bs.modal', function(){
+        Livewire.emit('resetForm');
+    });
+
+    window.addEventListener('hideAddCmaModal', function(event){
+      $('#create_cma-modal').modal('hide');
+    });
+
+    window.addEventListener('showEditCmaModal', function(event){
+      $('#edit_cma-modal').modal('show');
+    });
+
+    window.addEventListener('hideEditCmaModal', function(event){
+      $('#edit_cma-modal').modal('hide');
+    });
+
+
+    window.addEventListener('showDeleteCmaModal', function(event){
+      $('#delete_cma-modal').modal('show');
+    });
+
+    window.addEventListener('hideDeleteCmaModal', function(event){
+      $('#delete_cma-modal').modal('hide');
+    });
+
+  </script>
+@endpush
