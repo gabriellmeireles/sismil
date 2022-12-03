349221974f899c7ecda686e49a190fc9511f4ed6
gabriellmeireles <gabriellmeireles@hotmail.com>
2022-12-02 23:48:20 -0300

    organizando a indentação

--- resources/views/livewire/admin/military-organization/military-organization.blade.php
+++ resources/views/livewire/admin/military-organization/military-organization.blade.php
@@ -20,7 +20,6 @@
             </div>
         </div>
     </div>
-
     <div class="col-12">
         <div class="card">
             <div class="card-body border-bottom py-3">
@@ -115,3 +114,4 @@
     @include('livewire.admin.military-organization.military-organization-create')
     @include('livewire.admin.military-organization.military-organization-edit')
     @include('livewire.admin.military-organization.military-organization-disable')
+</div>
