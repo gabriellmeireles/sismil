2dac779a511730e3ad0b0b75eb9a5e04dfb54ce4
gabriellmeireles <gabriellmeireles@hotmail.com>
2022-12-21 16:11:17 -0300

    exigindo a verificação de email do user candidato

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
+
     public function index()
     {
        return view('user.user.index');
