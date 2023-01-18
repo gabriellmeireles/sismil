b77839a92145ce9c3dcb873bb35b59a162ec4386
gabriellmeireles <gabriellmeireles@hotmail.com>
2023-01-18 11:50:28 -0300

    criando o model e migration do Froms

---
+++ app/Models/Form.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Form extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'combar_arm',
+        'military_organization',
+        'course_score',
+        'graduation_class',
+        'year_service_time',
+        'month_service_time',
+        'day_service_time',
+        'last_rank_degree',
+        'punishment',
+        'punishment_level',
+        'candidate_id',
+        'contest_area_id',
+    ];
+
+    public function contestArea(){
+        return $this->belongsTo(ContestArea::class, 'contest_area_id', 'id');
+    }
+
+    public function candidate(){
+        return $this->belongsTo(Candidate::class, 'candidate_id', 'id');
+    }
+
+
+}
---
+++ database/migrations/2023_01_18_105459_create_forms_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('combat_arm');
+            $table->string('military_organization');
+            $table->float('course_score');
+            $table->string('graduation_class');
+            $table->tinyInteger('year_service_time');
+            $table->tinyInteger('month_service_time');
+            $table->tinyInteger('day_service_time');
+            $table->string('last_rank_degree');
+            $table->string('punishment');
+            $table->string('punishment_level');
+            $table->foreignId('candidate_id')->constrained('candidates','id');
+            $table->foreignId('contest_area_id')->constrained('contest_areas','id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+};
